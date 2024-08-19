da6ff55637c9ba960d83aba721e7a5344259b031
Cesar Fernandez <65238013+CxA2003@users.noreply.github.com>
2024-08-19 11:17:41 -0400

    Added files via upload
    Uploaded files from local, except from the exported mode (due to large file size)

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# weapon-identification
-Data Science project, using VGG16 to make a weapon idenfication app on Streamlit
+# Weapon Identifier
+
+## Project Overview
+
+The **Weapon Identifier** is a Streamlit app designed to help users determine if a gun is present in their images. The application utilizes a machine learning model to analyze the content of images and trigger an alert if a weapon is detected. This tool is intended to be used for educational and informational purposes.
+
+## Dataset
+
+The dataset used for this project is composed of images extracted from the [OpenImages website](https://storage.googleapis.com/openimages/web/index.html) using the [openimages Python library](https://pypi.org/project/openimages/). These images were carefully selected and categorized to train the model in distinguishing between images containing guns and those without.
+
+## Model Details
+
+This project employs the [VGG16](https://keras.io/api/applications/vgg/#vgg16-function) model, a well-known convolutional neural network architecture. The model has been adapted with additional layers, including [GlobalAveragePooling2D](https://www.tensorflow.org/api_docs/python/tf/keras/layers/GlobalAveragePooling2D) and [BatchNormalization](https://keras.io/api/layers/normalization_layers/batch_normalization/#batchnormalization-class), to enhance its performance in this specific task. The training process was optimized using [early stopping](https://keras.io/api/callbacks/early_stopping/#earlystopping-class) to prevent overfitting and ensure the model's generalizability.
+
+## Installation and Usage
+
+No specific installation is required to run this project. The entire process can be executed using web-based tools like [Kaggle Notebooks](https://www.kaggle.com/code) and [Google Colab](https://colab.research.google.com/). Detailed instructions and notebooks are provided to guide users through running the application in these environments.
+
+#### Steps to run on web-based tools:
+
+- Notebook for the model:
+    - Upload the **WeaponIdentifyier(Kaggle Notebooks).ipynb** file to your Kaggle Account ([tutorial here](https://www.youtube.com/watch?v=HaeoKp0akN0&t=4s))
+    - Set the Accelerator to GPU T4 x2
+    - Run the whole notebook
+    - The resulting model should appear on the right side of the page, under the "Output" section
+- Notebook for the Streamlit App:
+    - Upload the **WeaponIdentifyier_streamlit_app(Colab).ipynb** file to your Google Drive, or directly to your Google Colab account ([tutorial here](https://www.youtube.com/watch?v=R3sKKvMCwTo))
+    - Upload the **app.py** and **weapon_class.h5** files to your Colab session files. To achieve this, click on the folder icon, then drag and drop the files, please wait until they finish loading
+    - Obtain your ngrok Authtoken:
+        - Log in/Sign up on the [ngrok website](https://ngrok.com/)
+        - Once on the Dashboard, head to the "Your Authtoken" section
+        - Copy your personal Authtoken
+    - Replace the `YOUR_AUTHTOKEN` with your own Authtoken
+    - Run the whole notebook
+    - The link to your running app will be displayed as the output of the last code block
+
+## Features
+
+- **Real-time Weapon Detection**: Upload an image, and the app will analyze it to determine if a gun is present.
+- **User-Friendly Interface**: The app is built using [Streamlit](https://streamlit.io/), ensuring a smooth and intuitive user experience.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for more details.
---
+++ app.py
@@ -0,0 +1,171 @@
+import streamlit as st
+import tensorflow as tf
+from PIL import Image
+import numpy as np
+
+st.title('Is there a weapon?')
+st.write('Streamlit app made by Cesar Fernandez')
+
+page = st.sidebar.selectbox(
+    'Page',
+    ('Home', 'Weapon Identifier')
+)
+
+
+base_css = """
+    <style>
+    .st-emotion-cache-h4xjwg {
+        background-color: #060606;
+        color: #ECECEC;
+    }
+    .st-emotion-cache-6qob1r {
+        background-color: #060606;
+    }
+    </style>
+    """
+
+st.markdown(base_css, unsafe_allow_html=True)
+
+if page == 'Home':
+    css_code = """
+    <style>
+    h1, h3 {
+        color: white
+    }
+    header {
+        background-color: #1e1e1e;
+        color: white;
+    }
+    .main {
+        background-color: #3e4172;
+        opacity: 1;
+        background: radial-gradient(circle, transparent 20%, #3e4172 20%, #3e4172 80%, transparent 80%, transparent), radial-gradient(circle, transparent 20%, #3e4172 20%, #3e4172 80%, transparent 80%, transparent) 10px 10px, linear-gradient(#000000 0.8px, transparent 0.8px) 0 -0.4px, linear-gradient(90deg, #000000 0.8px, #3e4172 0.8px) -0.4px 0;
+        background-size: 20px 20px, 20px 20px, 10px 10px, 10px 10px;
+    }
+    .st-emotion-cache-1n76uvr {
+        background-color: #1e1e1e;
+        color: white;
+        display: flex;
+        align-items: center;
+        text-align: center;
+        padding: 3rem;
+        border-radius: 20px;
+        box-shadow: 0 0 10px #a8c0ff;
+    }
+    .st-emotion-cache-13ln4jf {
+        width: 100%;
+        padding: 6rem 1rem 6rem;
+        max-width: 46rem;
+    }
+    .st-emotion-cache-1sno8jx img {
+        border-radius: 10px;
+    }
+    </style>
+    """
+
+    st.markdown(css_code, unsafe_allow_html=True)
+    st.subheader('About this project')
+    st.write('''
+This Streamlit app allows the user to upload images and determine if they contain
+a weapon with a model that reached a 99% precision.
+    ''')
+    st.markdown("[![Foo](https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iAhX.YnazclY/v1/-1x-1.jpg)]()")
+
+if page == 'Weapon Identifier':
+    css_code = """
+    <style>
+    h1, h3 {
+        color: white
+    }
+    header {
+        background-color: #1e1e1e;
+        color: white;
+    }
+    .main {
+        background-color: #3e4172;
+        opacity: 1;
+        background: radial-gradient(circle, transparent 20%, #3e4172 20%, #3e4172 80%, transparent 80%, transparent), radial-gradient(circle, transparent 20%, #3e4172 20%, #3e4172 80%, transparent 80%, transparent) 10px 10px, linear-gradient(#000000 0.8px, transparent 0.8px) 0 -0.4px, linear-gradient(90deg, #000000 0.8px, #3e4172 0.8px) -0.4px 0;
+        background-size: 20px 20px, 20px 20px, 10px 10px, 10px 10px;
+    }
+    .st-emotion-cache-1n76uvr {
+        background-color: #1e1e1e;
+        color: white;
+        display: flex;
+        align-items: center;
+        text-align: center;
+        padding: 3rem;
+        border-radius: 20px;
+        box-shadow: 0 0 10px #a8c0ff;
+    }
+    .st-emotion-cache-13ln4jf {
+        width: 100%;
+        padding: 6rem 1rem 6rem;
+        max-width: 46rem;
+    }
+    .st-emotion-cache-1sno8jx img {
+        border-radius: 10px;
+    }
+    .st-emotion-cache-qgowjl p{
+        color:white;
+        padding-left: 1rem;
+    }
+    .st-emotion-cache-7oyrr6, .st-emotion-cache-9ycgxx {
+        color: white;
+    }
+    .st-emotion-cache-1erivf3 {
+        background-color: transparent;
+    }
+    .st-emotion-cache-15hul6a {
+        background-color: #1e1e1e;
+        color: white;
+        border-color: #1e1e1e;
+    }
+    .st-emotion-cache-15hul6a::hover {
+        background-color: #1e1e1e;
+        color: white
+    }
+    .st-emotion-cache-1v0mbdj {
+        margin: 0 auto;
+    }
+    .st-emotion-cache-7oyrr6, .st-emotion-cache-9ycgxx {
+      color: white;
+    }
+    </style>
+    """
+
+    st.markdown(css_code, unsafe_allow_html=True)
+    st.subheader("Model testing")
+    st.write('For now, the model is only compatible with .jpg images.')
+    uploaded_file = st.file_uploader(
+        label="File uploading section",
+        type=['jpg'],
+        accept_multiple_files=False
+    )
+
+    if uploaded_file is not None:
+        # Load the model
+        model = tf.keras.models.load_model('/content/weapon_class.h5')
+        model.summary()
+
+        # Convert the uploaded file to an image
+        img = Image.open(uploaded_file)
+        st.image(img, caption="Uploaded by user", width=500)
+
+        # Preprocess the image
+        img = img.resize((224, 224, 3))
+        img_array = np.array(img)
+        img_array = tf.keras.applications.resnet.preprocess_input(img_array)
+        img_array = np.expand_dims(img_array, axis=0)
+
+        # Predict the result
+        prediction = model.predict(img_array)
+        class_label = np.argmax(prediction, axis=1)[0]
+        probability = np.max(prediction)
+
+        st.write("And the result is...")
+        if class_label == 0:
+            st.write(f"⚠️ Weapon detected! (Probability: {probability:.2f})")
+        else:
+            st.write(f"✅ No weapon detected. (Probability: {probability:.2f})")
+    else:
+        st.write("Please, upload an image!")
